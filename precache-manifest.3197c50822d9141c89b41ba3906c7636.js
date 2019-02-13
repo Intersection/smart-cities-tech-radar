482ef62a0df7fe310d30799baa825850fecb27d2
IxN Buildkite <buildkite@intersection.com>
2019-02-13 18:24:32 +0000

    Updates

---
+++ precache-manifest.3197c50822d9141c89b41ba3906c7636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8ef48ff5367987ca004",
+    "url": "/smart-cities-tech-radar/static/js/runtime~main.b8ef48ff.js"
+  },
+  {
+    "revision": "ded698f3b3f580b3cdfb",
+    "url": "/smart-cities-tech-radar/static/js/main.ded698f3.chunk.js"
+  },
+  {
+    "revision": "70d1d106ef9327aeccd7",
+    "url": "/smart-cities-tech-radar/static/js/1.70d1d106.chunk.js"
+  },
+  {
+    "revision": "ded698f3b3f580b3cdfb",
+    "url": "/smart-cities-tech-radar/static/css/main.7b53d6c7.chunk.css"
+  },
+  {
+    "revision": "0db29c61e827bdd3cb05427fea62a761",
+    "url": "/smart-cities-tech-radar/index.html"
+  }
+];
